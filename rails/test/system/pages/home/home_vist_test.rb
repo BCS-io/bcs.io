fa97a93ade70014355a48180712b12b401adfab5
Richard <richard.wigley@gmail.com>
2019-10-13 18:01:20 +0100

    Rails +simple visit test

---
+++ rails/test/system/pages/home/home_vist_test.rb
@@ -0,0 +1,8 @@
+require 'application_system_test_case'
+
+class HomeVisitTest < ApplicationSystemTestCase
+   test "visit index" do
+    visit root_url
+    assert_selector "h1", text: 'We are developers'
+  end
+end
