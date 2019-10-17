6a5699ddc16d968d1b5f088b4e9c80195dda4fa4
Richard <richard.wigley@gmail.com>
2019-10-17 17:15:36 +0100

    Rails +client side validations
     - gem 'client_side_validations'
     - yarn add @client-side-validations/client-side-validations
     - require client-side-validations

--- rails/app/javascript/packs/application.js
+++ rails/app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("jquery")
 
 require("packs/slide")
 
+// Client Side Validation
+require("@client-side-validations/client-side-validations")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
