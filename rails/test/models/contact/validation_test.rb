05bd90f4be89e62e499843d8b45eb3196749a428
Richard <richard.wigley@gmail.com>
2019-10-22 12:15:25 +0100

    Rails +contact form model
      - ActiveModel::Model - aka not database backed
      - contact model wraps the contact form data ready for the mailer

---
+++ rails/test/models/contact/validation_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class ContactTest < ActiveSupport::TestCase
+  test "#name" do
+    contact = Contact.new
+    assert_must validate_presence_of(:name), contact
+    assert_must validate_length_of(:name).is_at_least(2).is_at_most(100), contact
+  end
+
+  test "#email" do
+    contact = Contact.new
+    assert_must validate_presence_of(:email), contact
+    assert_must allow_value("email@example.com").for(:email), contact
+  end
+
+  test "#message" do
+    contact = Contact.new
+    assert_must validate_presence_of(:message), contact
+    assert_must validate_length_of(:message).is_at_least(5).is_at_most(2048), contact
+  end
+end
