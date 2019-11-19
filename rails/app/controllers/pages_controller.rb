1d423a2f5eb6fed914592f11118dc61c5d4edc88
Richard <richard.wigley@gmail.com>
2019-11-19 19:01:49 +0000

    Rails frontend pages#home
      - responsible for the default home, about, contact pages
      - https://stackoverflow.com/questions/349743/welcome-home-page-in-ruby-on-rails-best-practice/11003820#11003820

---
+++ rails/app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# PagesController
+#  - responsible for the standard webpages
+#    - home, about, contact etc
+#
+class PagesController < ApplicationController
+  def home; end
+end
