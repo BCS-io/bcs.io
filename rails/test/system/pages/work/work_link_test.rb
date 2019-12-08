7889899e7e95114e97b4d957f8dc7a651759c5dc
Richard <richard.wigley@gmail.com>
2019-12-08 14:51:11 +0000

    Rails ^work work links

---
+++ rails/test/system/pages/work/work_link_test.rb
@@ -0,0 +1,15 @@
+require 'application_system_test_case'
+
+class WorkLinkTest < ApplicationSystemTestCase
+  test "check links work" do
+    links = { 'svg.test_letting' => 'https://github.com/BCS-io/letting',
+              'svg.test_bcs_io' => 'https://github.com/BCS-io/bcs.io' }
+
+    links.to_a.each do |link|
+      visit work_pages_url
+      find(:css, link[0]).click
+      assert_current_path(link[1])
+    end
+  end
+end
+
