69156635bde113c357507f40af503a66717cd08e
Richard <richard.wigley@gmail.com>
2020-01-07 13:33:31 +0000

    Rails helpers +render_svg as inline_svg wrapper

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_svg(name, styles: "", title: nil)
+    filename = "#{name}.svg"
+    title ||= name.underscore.humanize
+    inline_svg(filename, aria: true, nocomment: true, title: title, class: styles)
+  end
 end
