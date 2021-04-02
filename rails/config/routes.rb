ebe1624deff2d9e770d7ebb2476824e14ee6b7aa
Richard <richard.wigley@gmail.com>
2021-04-02 15:28:13 +0100

    Rails add devise

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :authors
   get '/check.txt', to: proc {[200, {}, ['simple_check']]}
   resource :contact, only: %i[create]
   root to: "pages#home"
